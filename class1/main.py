c4b141ad69f53893dcac381133959bcae9a30f2f
eric20150705 <chiangeric0705@gmail.com>
2025-09-24 18:50:28 +0800

    新增主控程式以控制LED燈的PWM輸出

--- class1/main.py
+++ class1/main.py
@@ -1,1 +1,10 @@
-print("hello world")
+from machine import Pin
+from time import sleep
+
+p2 = Pin(2, Pin.OUT)
+
+while True:
+    p2.value(0)
+    sleep(1)
+    p2.value(1)
+    sleep(1)
---
+++ class1/prj01.py
@@ -0,0 +1,17 @@
+################################輸入模組################################
+from machine import Pin, PWM
+from time import sleep
+
+################################宣告與設定################################
+frequency = 1000
+duty_cycle = 0
+led = PWM(Pin(2), freq=frequency, duty=duty_cycle)
+
+################################主程式################################
+while True:
+    led.duty(0)
+    sleep(2)
+    led.duty(512)
+    sleep(2)
+    led.duty(1023)
+    sleep(2)
